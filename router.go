5db0a51ef77ad79898c7359ed5c726245fc9feb3
SallyHabib <sally.hanen@student.guc.edu.eg>
2017-11-01 21:59:13 +0200

    testing

--- router.go
+++ router.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "github.com/julienschmidt/httprouter"
-    "net/http"
-	"log"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprint(w, "Welcome! Andrew \n")
+	fmt.Fprint(w, "Welcome! zoo\n")
 }
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
 func Index2(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	response, err := http.Get("http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=216693&t.k=bhhitzZ6DNo&action=employers&q=pharmaceuticals&userip=192.168.43.42&useragent=Mozilla/%2F4.0")
-    responseData, err := ioutil.ReadAll(response.Body)
+	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintf(w,string(responseData))
+	fmt.Fprintf(w, string(responseData))
 }
 
 func main() {
-    router := httprouter.New()
+	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/glassdoor", Index2)
-    router.GET("/hello/:name", Hello)
+	router.GET("/hello/:name", Hello)
 
-    log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
