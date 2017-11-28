b9d453fa53ab9163aaed9c7fa1193f8735bf1ad9
SallyHabib <sally.hanen@student.guc.edu.eg>
2017-11-28 23:23:07 +0200

    feat: server returns whole response instead of urls

--- controller/controller.go
+++ controller/controller.go
@@ -146,7 +146,7 @@ func SearchForGlobalJobsGlassdoor(searchWord string, job string, country string)
 }
 
 // HandleJobs ... function
-func HandleJobs(session models.Session, input string) (string, error) {
+func HandleJobs(session models.Session, input string) (string, models.Response, error) {
 	_, arrayFound := session["preferences"]
 	if !arrayFound {
 		var array []string
@@ -158,25 +158,26 @@ func HandleJobs(session models.Session, input string) (string, error) {
 	}
 	counter, _ := session["counter"].(int)
 	userInputs, _ := session["preferences"].([]string)
+	resp := models.Response{}
 
 	switch counter {
 	case 0:
 		counter++
 		session["counter"] = counter
-		return "What field are you interested in?", nil
+		return "What field are you interested in?", resp, nil
 	case 1:
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		session["preferences"] = userInputs
 		counter++
 		session["counter"] = counter
-		return "Are you looking for a job or an internship?", nil
+		return "Are you looking for a job or an internship?", resp, nil
 	case 2:
 		userInputs = append(userInputs, input)
 		session["preferences"] = userInputs
 		counter++
 		session["counter"] = counter
-		return "which country?", nil
+		return "which country?", resp, nil
 	case 3:
 		countries := getCountries()
 		found := false
@@ -189,33 +190,33 @@ func HandleJobs(session models.Session, input string) (string, error) {
 		}
 
 		if !found {
-			return "Please enter a valid country", nil
+			return "Please enter a valid country", resp, nil
 		}
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		fmt.Println(userInputs[0], userInputs[1], userInputs[2])
 		messageResp := models.Response{}
-		messageResp2 := models.Response{}
+		//	messageResp2 := models.Response{}
 
 		var err error
 		if strings.ToLower(userInputs[2]) == "egypt" {
 			messageResp, err = SearchForLocalJobs(userInputs[0], userInputs[1], userInputs[2])
-			messageResp2, err = SearchForLocalJobsWuzzuf(userInputs[0], userInputs[1], userInputs[2])
+			//	messageResp2, err = SearchForLocalJobsWuzzuf(userInputs[0], userInputs[1], userInputs[2])
 		} else {
 			messageResp, err = SearchForGlobalJobs(userInputs[0], userInputs[1], userInputs[2])
-			messageResp2, err = SearchForGlobalJobsGlassdoor(userInputs[0], userInputs[1], userInputs[2])
+			//	messageResp2, err = SearchForGlobalJobsGlassdoor(userInputs[0], userInputs[1], userInputs[2])
 		}
-		message := Urls(messageResp)
-		message2 := Urls(messageResp2)
+		//	message := Urls(messageResp)
+		//message2 := Urls(messageResp2)
 
 		userInputs = userInputs[:0]
 		session["preferences"] = userInputs
 		counter = 0
 		session["counter"] = counter
-		resultReturn := message + message2
-		return resultReturn, err
+		//	resultReturn := message + message2
+		return "", messageResp, err
 	}
-	return "", nil
+	return "", resp, nil
 }
 
 // SearchForCourses ... function
@@ -238,7 +239,7 @@ func SearchForCourses(searchWord string, kind string) (models.Response, error) {
 }
 
 // HandleCourses ... function
-func HandleCourses(session models.Session, input string) (string, error) {
+func HandleCourses(session models.Session, input string) (string, models.Response, error) {
 	_, arrayFound := session["courses"]
 	if !arrayFound {
 		var array []string
@@ -250,32 +251,32 @@ func HandleCourses(session models.Session, input string) (string, error) {
 	}
 	counter, _ := session["coursesCounter"].(int)
 	userInputs, _ := session["courses"].([]string)
-
+	resp := models.Response{}
 	switch counter {
 	case 0:
 		counter++
 		session["coursesCounter"] = counter
-		return "What course you want to learn?", nil
+		return "What course you want to learn?", resp, nil
 	case 1:
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		session["courses"] = userInputs
 		counter++
 		session["coursesCounter"] = counter
-		return "Are you searching for beginner, intermediate or advanced course?", nil
+		return "Are you searching for beginner, intermediate or advanced course?", resp, nil
 	case 2:
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		fmt.Println(userInputs[0], userInputs[1])
 		messageResp, err := SearchForCourses(userInputs[0], userInputs[1])
-		message := Urls(messageResp)
+		//message := Urls(messageResp)
 		userInputs = userInputs[:0]
 		session["courses"] = userInputs
 		counter = 0
 		session["coursesCounter"] = counter
-		return message, err
+		return "", messageResp, err
 	}
-	return "", nil
+	return "", resp, nil
 }
 
 // SearchForDegrees ... function
@@ -299,7 +300,7 @@ func SearchForDegrees(searchWord string, kind string, country string) (models.Re
 }
 
 // HandleDegrees ... function
-func HandleDegrees(session models.Session, input string) (string, error) {
+func HandleDegrees(session models.Session, input string) (string, models.Response, error) {
 	_, arrayFound := session["degrees"]
 	if !arrayFound {
 		var array []string
@@ -311,25 +312,26 @@ func HandleDegrees(session models.Session, input string) (string, error) {
 	}
 	counter, _ := session["degreesCounter"].(int)
 	userInputs, _ := session["degrees"].([]string)
+	resp := models.Response{}
 
 	switch counter {
 	case 0:
 		counter++
 		session["degreesCounter"] = counter
-		return "What topic are you interested in?", nil
+		return "What topic are you interested in?", resp, nil
 	case 1:
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		session["degrees"] = userInputs
 		counter++
 		session["degreesCounter"] = counter
-		return "Are you looking for a Bachelor , Masters or PHD?", nil
+		return "Are you looking for a Bachelor , Masters or PHD?", resp, nil
 	case 2:
 		userInputs = append(userInputs, input)
 		session["degrees"] = userInputs
 		counter++
 		session["degreesCounter"] = counter
-		return "which country?", nil
+		return "which country?", resp, nil
 	case 3:
 		countries := getCountries()
 		found := false
@@ -342,37 +344,38 @@ func HandleDegrees(session models.Session, input string) (string, error) {
 		}
 
 		if !found {
-			return "Please enter a valid country", nil
+			return "Please enter a valid country", resp, nil
 		}
 
 		newInput := strings.Replace(input, " ", "%20", -1)
 		userInputs = append(userInputs, newInput)
 		fmt.Println(userInputs[0], userInputs[1], userInputs[2])
 		messageResp, err := SearchForDegrees(userInputs[0], userInputs[1], userInputs[2])
-		message := Urls(messageResp)
+		//	message := Urls(messageResp)
 		userInputs = userInputs[:0]
 		session["degrees"] = userInputs
 		counter = 0
 		session["degreesCounter"] = counter
-		return message, err
+		return "", messageResp, err
 	}
-	return "", nil
+	return "", resp, nil
 }
 
 // HandleSequence ... function
-func HandleSequence(session models.Session, input string) (string, error) {
+func HandleSequence(session models.Session, input string) (string, models.Response, error) {
 	var message string
 	var err error
+	resp := models.Response{}
 
 	for _, c := range Thanking {
 		if strings.ToLower(input) == strings.ToLower(c) {
-			return "7byby teslam", nil
+			return "7byby teslam", resp, nil
 		}
 	}
 
 	for _, c := range Goodbyes {
 		if strings.ToLower(input) == strings.ToLower(c) {
-			return "Bye", nil
+			return "Bye", resp, nil
 		}
 	}
 
@@ -396,7 +399,7 @@ func HandleSequence(session models.Session, input string) (string, error) {
 			"1) Jobs & Internships (type jobs)<br>" +
 			"2) Courses (type courses)<br>" +
 			"3) Bachelor, Masters & PHD Degrees (type degrees)"
-		return choices, nil
+		return choices, resp, nil
 	case 1:
 		switch strings.ToLower(input) {
 		case "jobs":
@@ -421,18 +424,18 @@ func HandleSequence(session models.Session, input string) (string, error) {
 			session["degrees"] = array
 			session["degreesCounter"] = 0
 
-			return choices, nil
+			return choices, resp, nil
 		}
 
 		switch scenario {
 		case 0:
-			message, err = HandleJobs(session, input)
+			message, resp, err = HandleJobs(session, input)
 		case 1:
-			message, err = HandleCourses(session, input)
+			message, resp, err = HandleCourses(session, input)
 		case 2:
-			message, err = HandleDegrees(session, input)
+			message, resp, err = HandleDegrees(session, input)
 		}
 
 	}
-	return message, err
+	return message, resp, err
 }
